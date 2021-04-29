58ed93dceeb1b787dc3023e2bdec3b560d39dba4
andresabcb <56406596+andresabcb@users.noreply.github.com>
2021-04-28 23:32:04 -0300

    5.5

--- client.js
+++ client.js
@@ -13,48 +13,101 @@ const tam_string = (str) => str.length;
 // EXERCÍCIO NOME USER
 const nome_user = (email) => email.split("@")[0];
 // EXERCÍCIO jaca-wars
+function jaca_wars(v, theta) {
+	let g = 9.8;
+	let dist = (Math.pow(v, 2) * Math.sin(2 * theta)) / g;
+	if (dist < 100 && dist + 2 < 100) {
+		return -1;
+	}
+	if (dist > 100 && dist - 2 > 100) {
+		return 1;
+	}
+	if (dist <= 102 || dist >= 98) {
+		return 0;
+	}
+}
 
 // EXERCÍCIO ano-bissexto
 // Uma das duas condições a seguir deve ser verdadeira:
 // Condição 1: Ser múltiplo de 4 e não ser múltiplo de 100
-// Condição 2: Ser múltiplo de 400 (se for múltiplo de 400 automaticamente é de 4) 
+// Condição 2: Ser múltiplo de 400 (se for múltiplo de 400 automaticamente é de 4)
 function anobiss(num) {
-    if ((num%4 == 0 && num%100 != 0) || num%400 == 0) {
-        return true
-    };
-    return false
-};
+	if ((num % 4 == 0 && num % 100 != 0) || num % 400 == 0) {
+		return true;
+	}
+	return false;
+}
 
 // EXERCÍCIO volume-da-pizza
-const vol_pizza = (raio_z,altura) => Math.round(Math.PI*Math.pow(raio_z,2)*altura);
+const vol_pizza = (raio_z, altura) =>
+	Math.round(Math.PI * Math.pow(raio_z, 2) * altura);
 
 // EXERCÍCIO mru
-const mru = (s0,v,t) => s0+v*t; 
+const mru = (s0, v, t) => s0 + v * t;
 
 // EXERCÍCIO inverte-string
-// inspirado em 
+// inspirado em
 // https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
 function inverte_string(string) {
-    let rts = "";
-    let i;
-    for (i = string.length - 1; i >= 0; i--) {
-        rts += string[i];
-    }
-    return rts;
-};
+	let rts = "";
+	let i;
+	for (i = string.length - 1; i >= 0; i--) {
+		rts += string[i];
+	}
+	return rts;
+}
 
 // EXERCÍCIO soma-valores
 // inspirado em
-// https://stackoverflow.com/questions/16449295/how-to-sum-the-values-of-a-javascript-object 
+// https://stackoverflow.com/questions/16449295/how-to-sum-the-values-of-a-javascript-object
 function soma_obj(objeto) {
-    let soma = 0;
-    for(var i in objeto) {
-      if(objeto.hasOwnProperty(i)) {
-        soma += parseFloat(objeto[i]); // retorna um float
-      }
-    }
-    return soma;
-  };
+	let soma = 0;
+	for (var i in objeto) {
+		if (objeto.hasOwnProperty(i)) {
+			soma += parseFloat(objeto[i]); // retorna um float
+		}
+	}
+	return soma;
+}
+
+// EXERCÍCIO soma-segundo-maior-e-menor-numeros
+// Maior e menor
+function soma_maior_e_menor(numbers) {
+	let num_anterior = 0;
+	let num;
+	let max = numbers[i];
+	let min = numbers[i];
+	let i;
+
+	for (i = 0; i < numbers.length; i++) {
+		num = numbers[i];
+		if (num > num_anterior) {
+			max = num;
+		}
+		if (num < num_anterior) {
+			min = num;
+		}
+		num_anterior = num;
+	}
+
+	let num_anterior2 = 0;
+	let num2;
+	let max2 = numbers[j];
+	let min2 = numbers[j];
+	let j;
+
+	for (let j = 0; j < numbers.length; j++) {
+		num2 = numbers[j];
+		if (num2 < max && num2 > num_anterior2) {
+			max2 = num2;
+		}
+		if (num2 > min && num2 < num_anterior2) {
+			min2 = num2;
+		}
+		num_anterior2 = num2;
+	}
+	return max2 + min2;
+}
 
 // EXERCÍCIO n-esimo-primo
 // function nprimo(num) {
@@ -62,7 +115,7 @@ function soma_obj(objeto) {
 //     let primo;
 //     if(num>1){
 //         for(i = 2;i < int(num/2); i++){
-//             if 
+//             if
 //         }
 //     }
 //     else{
@@ -100,100 +153,149 @@ axios
 				console.log(exercicios);
 				// ENVIANDO AS SUBMISSÕES
 
-                //EXERCÍCIO SOMA
+				//EXERCÍCIO SOMA
 				let entrada1_soma = exercicios["soma"]["entrada"]["a"];
-                let entrada2_soma = exercicios["soma"]["entrada"]["b"];
+				let entrada2_soma = exercicios["soma"]["entrada"]["b"];
 				let resultado_soma = soma(entrada1_soma, entrada2_soma);
-                console.log(resultado_soma);
-				axios
-					.post(
-						"https://tecweb-js.insper-comp.com.br/exercicio/soma",
-						{"resposta": resultado_soma },
-						options2
-					);
+				console.log(resultado_soma);
+				axios.post(
+					"https://tecweb-js.insper-comp.com.br/exercicio/soma",
+					{ resposta: resultado_soma },
+					options2
+				);
 
-                // EXERCÍCIO TAM STRING
-                let entrada1_tamstring = exercicios["tamanho-string"]["entrada"]["string"];
+				// EXERCÍCIO TAM STRING
+				let entrada1_tamstring =
+					exercicios["tamanho-string"]["entrada"]["string"];
 				let resultado_tamstring = tam_string(entrada1_tamstring);
-                console.log(resultado_tamstring);
-				axios
-					.post(
-						"https://tecweb-js.insper-comp.com.br/exercicio/tamanho-string",
-						{"resposta": resultado_tamstring },
-						options2
-					);
-                
-                // EXERCÍCIO nome-do-usuario
-                let entrada1_nomeuser = exercicios["nome-do-usuario"]["entrada"]["email"];
-                let resultado_nomeuser = nome_user(entrada1_nomeuser);
-                console.log(resultado_nomeuser);
-                axios
-                    .post(
-                        "https://tecweb-js.insper-comp.com.br/exercicio/nome-do-usuario",
-                        {"resposta": resultado_nomeuser },
-                        options2
-                    );
+				console.log(resultado_tamstring);
+				axios.post(
+					"https://tecweb-js.insper-comp.com.br/exercicio/tamanho-string",
+					{ resposta: resultado_tamstring },
+					options2
+				);
 
-                // EXERCÍCIO ano-bissexto
-                let entrada1_anobiss = exercicios["ano-bissexto"]["entrada"]["ano"];
-                let resultado_anobiss = anobiss(entrada1_anobiss);
-                console.log(resultado_anobiss);
-                axios
-                    .post(
-                        "https://tecweb-js.insper-comp.com.br/exercicio/ano-bissexto",
-                        {"resposta": resultado_anobiss },
-                        options2
-                    );
-                // EXERCÍCIO volume-da-pizza
-                let entrada1_volpizza = exercicios["volume-da-pizza"]["entrada"]["z"];
-                let entrada2_volpizza = exercicios["volume-da-pizza"]["entrada"]["a"];
-                let resultado_volpizza = vol_pizza(entrada1_volpizza, entrada2_volpizza);
-                console.log(resultado_volpizza);
-                axios
-                    .post(
-                        "https://tecweb-js.insper-comp.com.br/exercicio/volume-da-pizza",
-                        {"resposta": resultado_volpizza },
-                        options2
-                    );
+				// EXERCÍCIO nome-do-usuario
+				let entrada1_nomeuser =
+					exercicios["nome-do-usuario"]["entrada"]["email"];
+				let resultado_nomeuser = nome_user(entrada1_nomeuser);
+				console.log(resultado_nomeuser);
+				axios.post(
+					"https://tecweb-js.insper-comp.com.br/exercicio/nome-do-usuario",
+					{ resposta: resultado_nomeuser },
+					options2
+				);
 
-                // EXERCÍCIO mru
-                let entrada1_mru = exercicios["mru"]["entrada"]["s0"];
-                let entrada2_mru = exercicios["mru"]["entrada"]["v"];
-                let entrada3_mru = exercicios["mru"]["entrada"]["t"];
-                let resultado_mru = mru(entrada1_mru, entrada2_mru, entrada3_mru);
-                console.log(resultado_mru);
-                axios
-                    .post(
-                        "https://tecweb-js.insper-comp.com.br/exercicio/mru",
-                        {"resposta": resultado_mru },
-                        options2
-                    );
+				// EXERCÍCIO jaca-wars
+				let entrada1_jacawars = exercicios["jaca-wars"]["entrada"]["v"];
+				let entrada2_jacawars = exercicios["jaca-wars"]["entrada"]["theta"];
+				let resultado_jacawars = jaca_wars(
+					entrada1_jacawars,
+					entrada2_jacawars
+				);
+				console.log(resultado_jacawars);
+				axios.post(
+					"https://tecweb-js.insper-comp.com.br/exercicio/jaca-wars",
+					{ resposta: resultado_jacawars },
+					options2
+				);
 
-                // EXERCÍCIO inverte-string
-                let entrada1_invertestr = exercicios["inverte-string"]["entrada"]["string"];
-                let resultado_invertestr = inverte_string(entrada1_invertestr);
-                console.log(resultado_invertestr);
-                axios
-                    .post(
-                        "https://tecweb-js.insper-comp.com.br/exercicio/inverte-string",
-                        {"resposta": resultado_invertestr },
-                        options2
-                    );
+				// EXERCÍCIO ano-bissexto
+				let entrada1_anobiss = exercicios["ano-bissexto"]["entrada"]["ano"];
+				let resultado_anobiss = anobiss(entrada1_anobiss);
+				console.log(resultado_anobiss);
+				axios.post(
+					"https://tecweb-js.insper-comp.com.br/exercicio/ano-bissexto",
+					{ resposta: resultado_anobiss },
+					options2
+				);
+				// EXERCÍCIO volume-da-pizza
+				let entrada1_volpizza = exercicios["volume-da-pizza"]["entrada"]["z"];
+				let entrada2_volpizza = exercicios["volume-da-pizza"]["entrada"]["a"];
+				let resultado_volpizza = vol_pizza(
+					entrada1_volpizza,
+					entrada2_volpizza
+				);
+				console.log(resultado_volpizza);
+				axios.post(
+					"https://tecweb-js.insper-comp.com.br/exercicio/volume-da-pizza",
+					{ resposta: resultado_volpizza },
+					options2
+				);
 
-                // EXERCÍCIO soma-valores
-                let entrada1_somaobj = exercicios["soma-valores"]["entrada"]["objeto"];
-                let resultado_somaobj = soma_obj(entrada1_somaobj);
-                console.log(resultado_somaobj);
-                axios
-                    .post(
-                        "https://tecweb-js.insper-comp.com.br/exercicio/soma-valores",
-                        {"resposta": resultado_somaobj },
-                        options2
-                    );
+				// EXERCÍCIO mru
+				let entrada1_mru = exercicios["mru"]["entrada"]["s0"];
+				let entrada2_mru = exercicios["mru"]["entrada"]["v"];
+				let entrada3_mru = exercicios["mru"]["entrada"]["t"];
+				let resultado_mru = mru(entrada1_mru, entrada2_mru, entrada3_mru);
+				console.log(resultado_mru);
+				axios.post(
+					"https://tecweb-js.insper-comp.com.br/exercicio/mru",
+					{ resposta: resultado_mru },
+					options2
+				);
 
-                // 
+				// EXERCÍCIO inverte-string
+				let entrada1_invertestr =
+					exercicios["inverte-string"]["entrada"]["string"];
+				let resultado_invertestr = inverte_string(entrada1_invertestr);
+				console.log(resultado_invertestr);
+				axios.post(
+					"https://tecweb-js.insper-comp.com.br/exercicio/inverte-string",
+					{ resposta: resultado_invertestr },
+					options2
+				);
+
+				// EXERCÍCIO soma-valores
+				let entrada1_somaobj = exercicios["soma-valores"]["entrada"]["objeto"];
+				let resultado_somaobj = soma_obj(entrada1_somaobj);
+				console.log(resultado_somaobj);
+				axios.post(
+					"https://tecweb-js.insper-comp.com.br/exercicio/soma-valores",
+					{ resposta: resultado_somaobj },
+					options2
+				);
+
+				// EXERCÍCIO soma-segundo-maior-e-menor-numeros
+				// let entrada1_soma2maioremenor = exercicios["soma-segundo-maior-e-menor-numeros"]["entrada"]["numeros"];
+				// let resultado_soma2maioremenor = soma_maior_e_menor(entrada1_soma2maioremenor);
+				// console.log(resultado_soma2maioremenor);
+				// axios
+				//     .post(
+				//         "https://tecweb-js.insper-comp.com.br/exercicio/soma-segundo-maior-e-menor-numeros",
+				//         {"resposta": resultado_soma2maioremenor },
+				//         options2
+				//     );
+
+				// EXERCÍCIO caca-ao-tesouro
+				// let entrada1_tesouro = exercicios["caca-ao-tesouro"]["entrada"]["inicio"];
+				// axios.get(
+				//     `${entrada1_tesouro}`,
+				//     options2
+				// ).then((tesouro) => {
+				//     if (tesouro)
+				// })
+				// let resultado_tesouro = soma_obj(entrada1_tesouro);
+				// console.log(resultado_tesouro);
+				// axios
+				//     .post(
+				//         "https://tecweb-js.insper-comp.com.br/exercicio/caca-ao-tesouro",
+				//         {"resposta": resultado_tesouro },
+				//         options2
+				//     );
 			});
 	});
+
+// EXERCÍCIO soma-segundo-maior-e-menor-numeros
+// let entrada1_soma2maioremenor = exercicios["soma-segundo-maior-e-menor-numeros"]["entrada"]["numeros"];
+// let resultado_soma2maioremenor = soma_maior_e_menor(entrada1_soma2maioremenor);
+// console.log(resultado_soma2maioremenor);
+// axios
+//     .post(
+//         "https://tecweb-js.insper-comp.com.br/exercicio/soma-segundo-maior-e-menor-numeros",
+//         {"resposta": resultado_soma2maioremenor },
+//         options2
+//     );
 
 // EXERCÍCIO generico
 // let slug = "generico"
@@ -218,19 +320,19 @@ axios
 
 // AUTOMATIZANDO AS RESPOSTAS - RASCUNHO (NÃO FUNCIONA)
 // .then((resposta) => {
-					// 	console.log(`A resposta era mesmo ${resposta}`);
-					// });
+// 	console.log(`A resposta era mesmo ${resposta}`);
+// });
 
-				// let f;
-				// for (f = 0; f < (funcoes_ex.length - 1);f++){
-				//     const slug = nome_ex[f];
-				//     let entrada = exercicios[slug]['entrada']
-				//     let resultado = funcoes_ex[f](entrada)
-				//     // .then(() => {})
-				//     let sucesso = axios.post(
-				//         `https://tecweb-js.insper-comp.com.br/exercicio/${slug}`,
-				//         {'resposta': resultado},
-				//         options1
-				//     ).then((resposta) => {
-				//         console.log(`A resposta era mesmo ${resposta}`);
-				//     });};
+// let f;
+// for (f = 0; f < (funcoes_ex.length - 1);f++){
+//     const slug = nome_ex[f];
+//     let entrada = exercicios[slug]['entrada']
+//     let resultado = funcoes_ex[f](entrada)
+//     // .then(() => {})
+//     let sucesso = axios.post(
+//         `https://tecweb-js.insper-comp.com.br/exercicio/${slug}`,
+//         {'resposta': resultado},
+//         options1
+//     ).then((resposta) => {
+//         console.log(`A resposta era mesmo ${resposta}`);
+//     });};
